d7ccae90d4167b0ad4b7ebcf9939ed0540349fca
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-08-02 09:58:38 -0300

    form1 y ZXing

--- Proyecto/PutToLightSoftware/PutToLightSoftware/Form1.cs
+++ Proyecto/PutToLightSoftware/PutToLightSoftware/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//
+using ZXing;
 
 namespace PutToLightSoftware
 {
@@ -15,6 +17,11 @@ namespace PutToLightSoftware
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
