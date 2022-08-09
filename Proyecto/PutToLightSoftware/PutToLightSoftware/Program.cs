2fa850dcd3a5ca12cea8e430c1c58ceddd8eca7d
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-08-09 08:09:45 -0300

    form de inicio

--- Proyecto/PutToLightSoftware/PutToLightSoftware/Program.cs
+++ Proyecto/PutToLightSoftware/PutToLightSoftware/Program.cs
@@ -16,7 +16,7 @@ namespace PutToLightSoftware
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Codificador());
+            Application.Run(new PutToLight());
         }
     }
 }
