df670be2cbf3027421df8c4b8dd42a34ea5a3cec
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-08-09 11:03:01 -0300

    Create variablesAprender.js

---
+++ JS-PutToLight/variablesAprender.js
@@ -0,0 +1,33 @@
+// tipos de datos
+
+"hello world" // string
+'hello world' // string
+
+1000000 // number
+    -
+    2.3
+
+// boolean
+true
+false
+
+// array
+    ["juan", "lucas", "marta"]
+    [1, 2, 3]
+    [true, false, true, true]
+
+// object
+
+{
+    "username": "juan",
+    "score": 70.4,
+    "hours": 14,
+    "professional": true
+}
+
+{
+    "username": "lucas",
+    "score": 0.4,
+    "hours": 1,
+    "professional": false
+}
