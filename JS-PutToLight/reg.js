076f29bfdb6da331e323d85c74aeb1480f97f7c8
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-08-29 15:55:21 -0300

    Update reg.js

--- JS-PutToLight/reg.js
+++ JS-PutToLight/reg.js
@@ -9,10 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
             if (KeyboardEvent.keyCode == "Enter") {
                 if (Pass1 === Pass2) {
                     //Acá poné el code para cambiar de pestaña al index
-                    <
-                    button class = "iniciar"
-                    type = "button"
-                    onclick = "window.open('index.html', '_self').focus()" > Registrarse < /button>
                 } else {
                     pan.style.color = "red";
                     pan.innerText = "Las contraseñas no coinciden";
