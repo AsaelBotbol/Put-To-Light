acdcf844af4a1fe6469db33afe71c04bb5a46287
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-10-25 10:39:04 -0300

    Update fetch.js

--- JS-PutToLight/fetch.js
+++ JS-PutToLight/fetch.js
@@ -1,12 +1,44 @@
-fetch('http://localhost/prueba.js',{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        usuario: "pepon",
-        contrasenia: "1234"
+fetch('http://localhost/prueba.js', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            usuario: "pepon",
+            contrasenia: "1234"
+        })
     })
-})
     .then(response => response.json())
     .then(data => console.log(data));
+
+//register
+enviarReg.addEventListener("click", () => {
+    fetch('http://localhost/prueba.js', {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email.value,
+            contrasenia: contrasenia.value,
+            nombre: nombreUsuario.value
+        })
+    })
+
+})
+
+//logIn
+document.querySelector(".Enviar").addEventListener("click", () => {
+    fetch('http://localhost/prueba.js', {
+        method: "post",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: document.querySelector("#iCorreo").value,
+            contrasenia: document.querySelector("#iContrasenia").value
+        })
+    })
+})
