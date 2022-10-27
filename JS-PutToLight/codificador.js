a71dba31871a5bf3c56cefa14273a29334d42287
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-10-27 14:38:54 -0300

    fetch en el codificador (no terminado)

--- JS-PutToLight/codificador.js
+++ JS-PutToLight/codificador.js
@@ -7,3 +7,27 @@ formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     QR.makeCode(formulario.link.value);
 });
+
+//codificador
+iniciar.addEventListener("click", () => {
+    fetch('http://localhost:9000/codificador.html', {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: Usu.value,
+                contrasenia: Pass1.value
+            })
+        })
+        .then(response => {
+            if (response.status === 200) {
+                // se creo el usuario
+                location.reload();
+            } else {
+                pan.innerText = "Ha ocurrido un error inesperado"
+                pan.style.color = "red";
+            }
+        })
+})
