424df7b22239558a125483a371801b3a87b1c879
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-11-01 14:32:47 -0300

    decodificador fetch

---
+++ JS-PutToLight/decodificador.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const result = document.getElementById("resultado");
+    const pan = document.getElementById("span3");
+    const btnsend = document.getElementById("dbsend");
+
+    btnsend.addEventListener("click", () => {
+        console.log("funca");
+        enviarForm();
+    });
+})
+
+
+
+function enviarForm() {
+    if (result !== "") {
+        pan.innerText = "No ingresaste ningun valor";
+        pan.style.color = "red";
+    }
+}
