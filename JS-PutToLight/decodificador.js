5ccef70a5f15438060f263b261d223e4f16e8da9
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-11-03 13:38:12 -0300

    fetch en el decodificador

--- JS-PutToLight/decodificador.js
+++ JS-PutToLight/decodificador.js
@@ -2,16 +2,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const result = document.getElementById("resultado");
     const pan = document.getElementById("span3");
     const btnsend = document.getElementById("dbsend");
+    const resultado = document.getElementsByClassName("result");
 
     btnsend.addEventListener("click", () => {
         console.log("CLICK");
         enviarForm();
     });
-})
 
-function enviarForm() {
-    if (result !== "") {
-        pan.innerText = "No ingresaste ningun valor";
-        pan.style.color = "red";
+    btnsend.addEventListener("click", () => {
+        console.log("CLICK");
+        enviarForm();
+    });
+
+    function enviarForm() {
+        if (result !== null) {
+            console.log("dou");
+            fetch('http://localhost:9000/decopp', {
+                    method: "POST",
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        nombre: result.value
+                    })
+                })
+                .then(response => {
+                    if (response.status === 200) {
+                        // se mandó la info
+                        console.log("se mandó la info");
+                        location.reload();
+                    } else {
+                        console.log("Ha ocurrido un error");
+
+                        pan.innerText = "Ha ocurrido un error inesperado"
+                        pan.style.color = "red";
+                    }
+                })
+        } else if (result === null) {
+            pan.innerText = "No ingresaste ningun valor";
+            pan.style.color = "red";
+        }
     }
-}
+})
